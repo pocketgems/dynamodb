596a157b019020a59dfdc2bcdea85ee2c04e6f59
Yu <yu@pocketgems.com>
2022-11-10 00:19:35 -0800

    Move to default port, add testing setup files, bump version

--- environment.js
+++ environment.js
@@ -1,5 +1,5 @@
 process.env.INDEBUGGER = 1
-process.env.DYNAMO_ENDPT = 'http://localhost:8100'
+process.env.DYNAMO_ENDPT = 'http://localhost:8000'
 process.env.AWS_ACCESS_KEY_ID = 'ignored'
 process.env.AWS_SECRET_ACCESS_KEY = 'ignored'
 process.env.SERVICE = 'unittest'
