e5fce47863c9b159b20a515887ec5e072c3fecaf
gemmer-zhendong <56006698+gemmer-zhendong@users.noreply.github.com>
2024-07-23 11:25:27 -0700

    [CI-000] Update to docker compose v2 (#26)

--- scripts/start-local-db.sh
+++ scripts/start-local-db.sh
@@ -2,5 +2,5 @@
 cd "`dirname \"$0\"`"
 
 if [ "`curl -s http://localhost:8000`" == "" ] || [ $# -ne 0 ]; then
-    COMPOSE_PROJECT_NAME='dynamodb' docker-compose up -d $@
+    COMPOSE_PROJECT_NAME='dynamodb' docker compose up -d $@
 fi
